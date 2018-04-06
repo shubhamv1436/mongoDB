7ea8d55fb67e2a3ad8afe09196e78525b66a5371
shubhamv1436 <shubhamv1436@gmail.com>
2018-04-06 18:34:06 +0530

    Add cookie support

--- fruit_form.py
+++ fruit_form.py
@@ -12,8 +12,14 @@ def favourite_fruit():
 	if (fruit == None or fruit == ""):
 		fruit="No fruit Selected"
 	
-	return bottle.template('fruit_selection.tpl',{'fruit':fruit})
+	bottle.response.set_cookie("fruit", fruit)
+    	bottle.redirect("/show_fruit")
 
+@bottle.route('/show_fruit')
+def show_fruit():
+    fruit = bottle.request.get_cookie("fruit")
+
+    return bottle.template('fruit_selection.tpl', {'fruit':fruit})
 
 bottle.debug(True)
 bottle.run(host="localhost",port=8082)
